a0aaeb0f307dae4f3789e55ae4d73a934eb6b613
Square-Face <sq8.square.face@gmail.com>
2023-12-01 11:03:37 +0100

    made cli for compiling shaders, building the project in optimization mode and running the lone executable with timing

---
+++ src/make.sh
@@ -0,0 +1,8 @@
+echo "\n==== compiling shader ====\n"
+./makelib.sh
+
+echo "\n==== building for release ====\n"
+cargo build --release
+
+echo "\n==== running with time ====\n"
+time ../target/release/PINS
