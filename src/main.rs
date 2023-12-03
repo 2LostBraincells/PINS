35b756b269aeda5583e6a78131961281713ec1f8
Square-Face <sq8.square.face@gmail.com>
2023-12-03 01:35:54 +0100

    runns a lot now

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@ mod parser;
 
 // Compiled metal lib 
 
-const START_YEAR: usize = 0;
-const START_MONTH: usize = 0;
-const START_DAY: usize = 0;
+const START_YEAR: u16 = 0;
+const START_MONTH: u16 = 0;
+const START_DAY: u16 = 0;
 
-const CHECKSUM: usize = 2454;
+const CHECKSUM: u16 = 2454;
 
-const YEARS: usize = 100;
-const MONTHS: usize = 100;
-const DAYS: usize = 100;
+const YEARS: u16 = 100;
+const MONTHS: u16 = 100;
+const DAYS: u16 = 100;
 
-const TOTAL: usize = YEARS * MONTHS * DAYS;
+const TOTAL: usize = YEARS as usize * MONTHS as usize * DAYS as usize;
 
 fn luhns(pin: [i32;10]) -> bool {
     //! Check a single PIN using the CPU
@@ -43,60 +43,76 @@ fn luhns_check() {
 }
 
 fn main() {
-    // Setup GPU
     let device = &gpu::get_device();
+
+    // Setup GPU
     let queue = device.new_command_queue();
-    let buffer = queue.new_command_buffer();
-    let encoder = buffer.new_compute_command_encoder();
+    for i in 0..10_000 {
+        let buffer = queue.new_command_buffer();
+        let encoder = buffer.new_compute_command_encoder();
+
+        // setup shader function
+        gpu::use_function(&device, "check_pin", encoder);
+
+        let mut offsets: [u16; 7] = [
+            START_YEAR,
+            START_MONTH,
+            START_DAY,
+            0,
+            YEARS,
+            MONTHS,
+            DAYS
+        ];
+
+        let length = offsets.len() as u64;
+        let size = length * core::mem::size_of::<u16>() as u64;
+
+        let buffer_a = device.new_buffer_with_data(
+            unsafe { mem::transmute(offsets.as_ptr()) }, // bytes
+            size, // length
+            MTLResourceOptions::StorageModeShared, // Storage mode
+        );
+
+        let buffer_result = device.new_buffer(
+            (TOTAL * core::mem::size_of::<bool>()) as u64, // length
+            MTLResourceOptions::StorageModeShared, // Storage mode
+        );
+
+        println!("Settings buffers");
+        encoder.set_buffer(0, Some(&buffer_a), 0);
+        encoder.set_buffer(1, Some(&buffer_result), 0);
+
+        let grid_size = metal::MTLSize::new(
+            YEARS as u64, //width
+            MONTHS as u64, // height
+            DAYS as u64); //depth
     
-    // setup shader function
-    gpu::use_function(&device, "check_pin", encoder);
-    
-    let offsets: [u16; 7] = [
-        START_YEAR  .try_into().unwrap(),
-        START_MONTH .try_into().unwrap(),
-        START_DAY   .try_into().unwrap(),
-        CHECKSUM    .try_into().unwrap(),
-        YEARS       .try_into().unwrap(),
-        MONTHS      .try_into().unwrap(),
-        DAYS        .try_into().unwrap()
-    ];
+        println!("{}", i);
 
-    let length = offsets.len() as u64;
-    let size = length * core::mem::size_of::<u16>() as u64;
+        
+        println!("Updating offsets buffers");
+        offsets[3] = i;
 
-    let buffer_a = device.new_buffer_with_data(
-        unsafe { mem::transmute(offsets.as_ptr()) }, // bytes
-        size, // length
-        MTLResourceOptions::StorageModeShared, // Storage mode
-    );
+        let a_ptr = buffer_a.contents() as *mut u16;
+        println!("Running unsafe");
+        unsafe { 
+            let ptr = (a_ptr).offset(3);
 
-    let buffer_result = device.new_buffer(
-        (TOTAL * core::mem::size_of::<bool>()) as u64, // length
-        MTLResourceOptions::StorageModeShared, // Storage mode
-    );
+            *ptr = i
+        }
 
+        println!("Dispatching threads");
+        encoder.dispatch_threads(grid_size, gpu::max_group());
 
-    encoder.set_buffer(0, Some(&buffer_a), 0);
-    encoder.set_buffer(1, Some(&buffer_result), 0);
+        println!("Commiting");
+        encoder.end_encoding();
+        buffer.commit();
+        buffer.wait_until_completed();
+        println!("Done computing");
 
-
-    let grid_size = metal::MTLSize::new(
-        YEARS.try_into().unwrap(), //width
-        MONTHS.try_into().unwrap(), // height
-        DAYS.try_into().unwrap()); //depth
-
-    encoder.dispatch_threads(grid_size, gpu::max_group());
-
-    encoder.end_encoding();
-    buffer.commit();
-    buffer.wait_until_completed();
-
-    let ptr = buffer_result.contents() as *const bool;
-    let len = buffer_result.length() as usize / mem::size_of::<bool>();
-    let slice = unsafe { slice::from_raw_parts(ptr, len) };
-
-    parser::print(&offsets, slice);
-
-
+        let ptr = buffer_result.contents() as *const bool;
+        let len = buffer_result.length() as usize / mem::size_of::<bool>();
+        let _slice = unsafe { slice::from_raw_parts(ptr, len) };
+    }
+    // parser::print(&offsets, slice);
 }
